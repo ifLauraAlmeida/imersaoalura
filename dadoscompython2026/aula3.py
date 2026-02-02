7b6116f11ad9dd57480c956035fa0220b781c070
ifLauraAlmeida <oddlauraink@hotmail.com>
2026-02-02 00:37:26 -0300

    feat: add seaborn bar chart ordered by average salary

--- dadoscompython2026/aula3.py
+++ dadoscompython2026/aula3.py
@@ -2,6 +2,7 @@ from utils import *
 from tratamento import *
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 pd.set_option("display.max_columns", None)
 
@@ -16,11 +17,21 @@ df_limpo = df.dropna()
 
 df_limpo = df_limpo.assign(ano=df_limpo["ano"].astype("int64"))
 
-df_limpo["senioridade"].value_counts().plot(
-    kind="bar", title="Distribuição de Senioridade"
-)
+# df_limpo["senioridade"].value_counts().plot(
+#     kind="bar", title="Distribuição de Senioridade"
+# )
+# plt.show()
 
+# sns.barplot(data=df_limpo, x='senioridade', y='usd')
+# plt.title("Media Salarial Por Senioridade")
+# plt.show()
+
+ordem = df_limpo.groupby("senioridade")["usd"].mean().sort_values(ascending=False).index
+
+plt.figure(figsize=(8,5))
+sns.barplot(data=df_limpo, x='senioridade', y='usd', order=ordem)
+plt.title("Media Salarial Por Senioridade")
+plt.xlabel("Nível de Senioridade")
+plt.ylabel("Sálario Médio Anual (USD)")
 plt.show()
-
-
 
