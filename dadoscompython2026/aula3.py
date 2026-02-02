ee42fc871ccf5a6c27ef4c48d33d70fe3ed56332
ifLauraAlmeida <oddlauraink@hotmail.com>
2026-02-02 00:13:33 -0300

    feat: create first dataframe visualization with seniority bar chart

--- dadoscompython2026/aula3.py
+++ dadoscompython2026/aula3.py
@@ -1,6 +1,7 @@
 from utils import *
 from tratamento import *
 import pandas as pd
+import matplotlib.pyplot as plt
 
 pd.set_option("display.max_columns", None)
 
@@ -13,7 +14,13 @@ novas_sub_categorias(df)
 
 df_limpo = df.dropna()
 
-df_limpo = df_limpo.assign(ano = df_limpo["ano"].astype("int64"))
+df_limpo = df_limpo.assign(ano=df_limpo["ano"].astype("int64"))
+
+df_limpo["senioridade"].value_counts().plot(
+    kind="bar", title="Distribuição de Senioridade"
+)
+
+plt.show()
 
 
 
