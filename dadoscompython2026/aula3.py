5908641604d7f0ab01bba1dcf11bf70b571f4160
ifLauraAlmeida <oddlauraink@hotmail.com>
2026-02-02 17:57:31 -0300

    feat: add pie chart analysis using plotly

--- dadoscompython2026/aula3.py
+++ dadoscompython2026/aula3.py
@@ -3,6 +3,7 @@ from tratamento import *
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
 
 pd.set_option("display.max_columns", None)
 
@@ -28,10 +29,50 @@ df_limpo = df_limpo.assign(ano=df_limpo["ano"].astype("int64"))
 
 ordem = df_limpo.groupby("senioridade")["usd"].mean().sort_values(ascending=False).index
 
-plt.figure(figsize=(8,5))
-sns.barplot(data=df_limpo, x='senioridade', y='usd', order=ordem)
-plt.title("Media Salarial Por Senioridade")
-plt.xlabel("Nível de Senioridade")
-plt.ylabel("Sálario Médio Anual (USD)")
-plt.show()
+# plt.figure(figsize=(8,5))
+# sns.barplot(data=df_limpo, x='senioridade', y='usd', order=ordem)
+# plt.title("Media Salarial Por Senioridade")
+# plt.xlabel("Nível de Senioridade")
+# plt.ylabel("Sálario Médio Anual (USD)")
+# plt.show()
+
+# plt.figure(figsize=(8,5))
+# sns.histplot(df_limpo['usd'],bins=40,kde=True) #bins - largura das barras kde - linha 
+# plt.title("Distribuição dos Salários Anuais")
+# plt.xlabel("Sálario em USD")
+# plt.ylabel("Frequência")
+# plt.show()
+
+# plt.figure(figsize=(8,5))
+# sns.boxplot(x=df_limpo['usd']) 
+# plt.title("Boxplot Salário")
+# plt.xlabel("Sálario em USD")
+# plt.show()
+
+# ordem_senioridade = ['Junior', 'Pleno', 'Senior', 'Executivo']
+# plt.figure(figsize=(8,5))
+# sns.boxplot(x='senioridade',y='usd', data=df_limpo, order=ordem_senioridade, palette='Set2', hue='senioridade')
+# plt.title("Boxplot Distribuição Salarial por Senioridade")
+# plt.xlabel("Sálario em USD")
+# plt.show()
+
+# senioridade_media_salario = df_limpo.groupby('senioridade')['usd'].mean().sort_values(ascending=False).reset_index()
+# fig = px.bar(senioridade_media_salario,
+#              x='senioridade',
+#              y='usd',
+#              title='Média Salarial por Senioridade',
+#              labels={'senioridade': 'Nível de Senioridade', 'usd': 'Média Salarial Anual (USD)'})
+# fig.show()
+
+# remoto_contagem = df_limpo['remoto'].value_counts().reset_index()
+# remoto_contagem.columns = ['tipo_trabalho','quantidade']
+
+# fig = px.pie(remoto_contagem,
+#              names='tipo_trabalho',
+#              values='quantidade',
+#              title='Proporção dos Tipos de Trabalho',
+#              hole=0.5)
+# fig.update_traces(textinfo='percent+label')
+# fig.show()
+##########################DESAFIO FIM DA AULA3
 
